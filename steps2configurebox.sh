21d05c475325e4e67b2b719ca564c4ec68448d3d
mymodroid182 <31724731+mymodroid182@users.noreply.github.com>
2018-01-10 11:34:28 +0100

    Update steps2configurebox.sh

--- steps2configurebox.sh
+++ steps2configurebox.sh
@@ -3,11 +3,11 @@ add-apt-repository ppa:webupd8team/atom -y
 apt-get update
 apt-get upgrade -y
 apt-get install xorg lightdm lightdm-gtk-greeter lxde-core p7zip-full firefox terminator python-pip atom xarchiver -y
-python2.7 -m pip install --upgrade pip
-python2.7 -m pip install pefile
+#python2.7 -m pip install --upgrade pip
+#python2.7 -m pip install pefile
 mkdir /opt/malware /opt/framework /opt/clean
 #Other Stuff To Do
 #/media/user/VBOXADDITIONS_5.1.26_117224/VBoxLinuxAdditions.run
-#reboot
+reboot
 #vi ~/.config/openbox/lxde-rc.xml
 #openbox-lxde --reconfigure
