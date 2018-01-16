1162ee7244a6dafe261a56d78b43a939a4604672
mymodroid182 <31724731+mymodroid182@users.noreply.github.com>
2018-01-16 11:29:46 +0100

    Update steps2configurebox.sh

--- steps2configurebox.sh
+++ steps2configurebox.sh
@@ -2,10 +2,16 @@
 add-apt-repository ppa:webupd8team/atom -y
 apt-get update
 apt-get upgrade -y
-apt-get install xorg lightdm lightdm-gtk-greeter lxde-core p7zip-full firefox terminator python-pip atom xarchiver -y
-#python2.7 -m pip install --upgrade pip
-#python2.7 -m pip install pefile
-mkdir /opt/malware /opt/framework /opt/clean
+apt-get install xorg lightdm lightdm-gtk-greeter lxde-core p7zip-full firefox terminator python-pip atom xarchiver python3-pip -y
+apt-get autoclean
+apt-get autoremove
+mkdir /opt/malware /opt/framework /opt/clean /opt/shared
+chown -R user /opt/framework/ /opt/shared/
+python3.5 -m pip install --upgrade pip
+for i in {pefile,pandas,sklearn,scipy}; do python3.5 -m pip install $i;done
+temp1="sig_f = open( filename, 'rt')"
+temp2="sig_f = open( filename, 'rt',encoding = 'Latin-1')"
+sed -i "s/$temp1/$temp2/g" /usr/local/lib//python3.5/dist-packages/peutils.py
 #Other Stuff To Do
 #/media/user/VBOXADDITIONS_5.1.26_117224/VBoxLinuxAdditions.run
 reboot
